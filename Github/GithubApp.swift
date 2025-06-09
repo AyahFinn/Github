95d31fe60f7a1d3493410fbd56e3c4851aa8bbfe
Ayah <ayah29393@gmail.com>
2025-06-09 09:22:31 -0500

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
